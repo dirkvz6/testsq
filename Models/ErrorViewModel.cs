39ab946b528bb54562f65ecd81a7081cf77bac20
dirkvz6 <dirkvanzyl3c@gmail.com>
2026-07-21 21:01:06 +0200

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace sanquote_demo.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
